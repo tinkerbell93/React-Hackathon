4105c9e93ffc3ce7277b2115a15b81b4b77ea669
SaltPepperSalt <dksro324@gmail.com>
2020-08-14 08:46:11 +0900

    Antd Row and COl

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ScheduleListContainer from '../containers/ScheduleListContainer'
+import { Row, Col } from 'antd'
 
 export default function Home() {
   return (
-    <ScheduleListContainer />
+    <Row justify="center">
+      <Col span={16}>
+        <ScheduleListContainer />
+      </Col>
+    </Row>
   )
 }
