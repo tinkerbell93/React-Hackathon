0e4252e3f249e8d48f06df7850d54b04bd812bae
SaltPepperSalt <dksro324@gmail.com>
2020-08-14 13:35:33 +0900

    test

--- src/components/ScheduleList.jsx
+++ src/components/ScheduleList.jsx
@@ -15,6 +15,7 @@ const defaultChars = { __html: '&#128566' }
 export default function ScheduleList({ schedule, loading, error, getSchedule }) {
   const today = new Date();
   const _today = '' + today.getMonth() + (today.getDate() < 10 ? '' + 0 + today.getDate() : today.getDate());
+  let curMonth = 0;
 
   React.useEffect(() => {
     getSchedule();
@@ -49,11 +50,14 @@ export default function ScheduleList({ schedule, loading, error, getSchedule }) 
 
 
   return (
-    <Calendar dateCellRender={dateCellRender} monthCellRender={8} onSelect={(value) => { select(value) }} />
+    <Calendar dateCellRender={dateCellRender} onSelect={(value) => { select(value) }} />
   );
 
   function select(value) {
     const [month, date] = [value._d.getMonth(), (value._d.getDate() < 10 ? '' + 0 + value._d.getDate() : value._d.getDate())];
+    console.log(month, curMonth);
+
+    if (month !== curMonth) return;
     const _date = '' + month + date;
     console.log(_date);
     history.push({
