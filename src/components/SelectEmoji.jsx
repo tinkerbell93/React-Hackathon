c510da5960800c1a1f26f17fd1d49353ab4ff200
tinkerbell93 <hhjj105@gmail.com>
2020-08-14 18:22:00 +0900

    feat: style scss select emoji page

--- src/components/SelectEmoji.jsx
+++ src/components/SelectEmoji.jsx
@@ -1,91 +1,91 @@
-import React from "react";
-import { history } from "../redux/create";
-import { Row, Col } from "antd";
-import { useSelector } from "react-redux";
-import styles from "../css/selectEmoji.module.scss";
+import React from 'react';
+import { history } from '../redux/create';
+import { useSelector } from 'react-redux';
+import styles from '../css/selectEmoji.module.scss';
 
 export default function SelectEmoji({ date }) {
   const schedule = useSelector((state) => state.schedule.schedule);
   const item = schedule.find((item) => item.author === date);
   return (
-    <Row justify="center">
-      <Col span={16} className={styles.container}>
-        <div className={styles["flex-container"]}>
+    <div className={styles['select-emogi']}>
+      <div className={styles['emogi-wrapper']}>
+        <h2>Select emotion!</h2>
+        <div className={styles['select-wrapper']}>
           <div
-            role="button"
-            className={styles["emoji-container"]}
+            role='button'
+            className={styles['emoji-container']}
             onClick={() => {
-              click("&#128566");
+              click('&#128566');
             }}
           >
-            <span className={styles.emoji} role="img" aria-label="기본">
+            <span className={styles.emoji} role='img' aria-label='기본'>
               &#128566;
             </span>
           </div>
           <div
-            role="button"
-            className={styles["emoji-container"]}
+            role='button'
+            className={styles['emoji-container']}
             onClick={() => {
-              click("&#128518");
+              click('&#128518');
             }}
           >
-            <span className={styles.emoji} role="img" aria-label="기쁨">
+            <span className={styles.emoji} role='img' aria-label='기쁨'>
               &#128518;
             </span>
           </div>
           <div
-            role="button"
-            className={styles["emoji-container"]}
+            role='button'
+            className={styles['emoji-container']}
             onClick={() => {
-              click("&#128546");
+              click('&#128546');
             }}
           >
-            <span className={styles.emoji} role="img" aria-label="슬픔">
+            <span className={styles.emoji} role='img' aria-label='슬픔'>
               &#128546;
             </span>
           </div>
           <div
-            role="button"
-            className={styles["emoji-container"]}
+            role='button'
+            className={styles['emoji-container']}
             onClick={() => {
-              click("&#128544");
+              click('&#128544');
             }}
           >
-            <span className={styles.emoji} role="img" aria-label="화남">
+            <span className={styles.emoji} role='img' aria-label='화남'>
               &#128544;
             </span>
           </div>
           <div
-            role="button"
-            className={styles["emoji-container"]}
+            role='button'
+            className={styles['emoji-container']}
             onClick={() => {
-              click("&#128524");
+              click('&#128524');
             }}
           >
-            <span className={styles.emoji} role="img" aria-label="만족">
+            <span className={styles.emoji} role='img' aria-label='만족'>
               &#128524;
             </span>
           </div>
           <div
-            role="button"
-            className={styles["emoji-container"]}
+            role='button'
+            className={styles['emoji-container']}
             onClick={() => {
-              click("&#128532");
+              click('&#128532');
             }}
           >
-            <span className={styles.emoji} role="img" aria-label="걱정">
+            <span className={styles.emoji} role='img' aria-label='걱정'>
               &#128532;
             </span>
           </div>
         </div>
-      </Col>
-    </Row>
+      </div>
+    </div>
   );
 
   function click(value) {
     if (item) {
       history.push({
-        pathname: "/edit",
+        pathname: '/edit',
         state: {
           schedule: item,
           emoji: value,
@@ -94,7 +94,7 @@ export default function SelectEmoji({ date }) {
       return;
     }
     history.push({
-      pathname: "/add",
+      pathname: '/add',
       state: {
         date,
         emoji: value,
